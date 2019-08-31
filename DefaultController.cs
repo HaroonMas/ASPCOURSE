58c7205d583377eeb8d03ad6293ba6dff2f81bbb
HaroonMas <54428731+HaroonMas@users.noreply.github.com>
2019-08-31 00:04:17 -0400

    Add files via upload

---
+++ DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeginningMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View("Home");
+        }
+        public ActionResult Semester()
+        {
+            return View("Semester5");
+        }
+        public ActionResult Testimonial()
+        {
+            return View("Testimonial");
+        }
+    }
+}
